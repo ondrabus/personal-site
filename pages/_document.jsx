dfa81dd7a8a61a7566fceb4ba72339863d1dedef
ondrejp <ondrejp@kentico.com>
2020-09-22 19:25:57 +0200

    Adds GA

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,6 +14,15 @@ class MyDocument extends Document {
                     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
                     <link href="/css/main.css" rel="stylesheet"/>
                     <link rel="icon" type="image/png"  href="/img/avatar.png" />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126970131-1"></script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'UA-126970131-1');`
+                    }} />
                 </Head>
                 <body>
                     <Main />
