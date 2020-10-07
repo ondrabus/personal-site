010a0ba411623f312634e7fa21233ff39cef1e65
ondrejp <ondrejp@kentico.com>
2020-10-07 20:51:48 +0200

    Enables preview for homepage

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -34,6 +34,6 @@ export default async function preview(req: NextApiRequest, res: NextApiResponse)
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/${req.query.codename}` })
+  res.writeHead(307, { Location: `/${req.query.codename != "homepage" ? req.query.codename : ''}` })
   res.end()
 }
